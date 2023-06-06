989b988fa460425c0d0c921b26b3c09b29a8e6fa
Ceyda Esen <ceyda666esen@gmail.com>
2023-06-06 15:27:39 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-\s']/g, "");
+  }
+
+  static titleize(string) {
+    const excludedWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const words = string.split(" ");
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
 }
+
+module.exports = Formatter;
